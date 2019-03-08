a84bf58c3361d0de96fc938b92639b49e25badca
huntto <huntto1990@gmail.com>
2019-03-08 18:56:20 +0800

    mix back

--- app/src/main/res/raw/texture_fragment_shader.glsl
+++ app/src/main/res/raw/texture_fragment_shader.glsl
@@ -4,11 +4,15 @@ uniform float uFlat;
 uniform sampler2D uTextureUnit;
 varying vec2 vTextureCoordinates;
 varying vec4 vBlendColor;
-
+varying float vIsMix;
 void main() {
     if (uFlat > 0.5) {
         gl_FragColor = texture2D(uTextureUnit, vTextureCoordinates);
     } else {
-        gl_FragColor = texture2D(uTextureUnit, vTextureCoordinates);
+        if (vIsMix > 0.5) {
+            gl_FragColor = mix(texture2D(uTextureUnit, vTextureCoordinates), vec4(1.0), 0.9);
+        } else {
+            gl_FragColor = texture2D(uTextureUnit, vTextureCoordinates);
+        }
     }
 }
--- app/src/main/res/raw/texture_vertex_shader.glsl
+++ app/src/main/res/raw/texture_vertex_shader.glsl
@@ -9,11 +9,16 @@ attribute vec2 aPosition;
 varying vec2 vTextureCoordinates;
 varying vec4 vBlendColor;
 
+const float BACK_Z = 1.0;
+const float FRONT_Z = 0.9;
+varying float vIsMix;
+
 void main() {
     vTextureCoordinates = vec2(aPosition.x / uSize.x, aPosition.y / uSize.y);
     if (uFlat > 0.5) {
-        gl_Position = uMatrix * vec4(-aPosition.x, aPosition.y, 1.0, 1.0);
+        gl_Position = uMatrix * vec4(-aPosition.x, aPosition.y, BACK_Z + 0.0001, 1.0);
     } else {
+        vec3 newPosition = vec3(aPosition.xy, BACK_Z);
         // 中点
         float x0 = (uDragPoint.x + uOriginPoint.x) / 2.0;
         float y0 = (uDragPoint.y + uOriginPoint.y) / 2.0;
@@ -26,19 +31,22 @@ void main() {
         float origin = (uOriginPoint.x - x0) * dragVec.x + (uOriginPoint.y - y0) * dragVec.y;
         // 再将当前点代入方程
         float current = (aPosition.x - x0) * dragVec.x + (aPosition.y - y0) * dragVec.y;
+
+        // 当前点到中垂线的距离
+        float dist = abs(current) / sqrt(dragVec.x * dragVec.x + dragVec.y * dragVec.y);
+
+        vIsMix = 0.0;
         // 如果origin和current符号相同，则在中垂线同侧，否则异侧
         if (origin * current > 0.0) {
             // 求相对于中垂线的对称点
-            // 1. 当前点到中垂线的距离
-            float dist = current / sqrt(dragVec.x * dragVec.x + dragVec.y * dragVec.y);
-            // 2. 求得拉拽方向的单位向量
-            vec2 normalizedDragVec = -normalize(dragVec);
-            // 3. 当前点移动到对称点位置
+            // 求得拉拽方向的单位向量
+            vec2 normalizedDragVec = normalize(dragVec);
+            // 当前点移动到对称点位置
             vec2 symmetric = aPosition + (dist * 2.0) * normalizedDragVec;
-            gl_Position = uMatrix * vec4(-symmetric.x, symmetric.y, 0.9998, 1.0);
-        } else {
-            gl_Position = uMatrix * vec4(-aPosition.x, aPosition.y, 0.9999, 1.0);
+            newPosition = vec3(symmetric.xy, FRONT_Z);
+            vIsMix = 1.0;
         }
 
+        gl_Position = uMatrix * vec4(-newPosition.x, newPosition.y, newPosition.z, 1.0);
     }
 }
